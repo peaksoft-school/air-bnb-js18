ea1525a6be3885eaf4520f4782e8709bb822d5cc
Ymanbek Baltabaev Eralievich <ymanbekbaltabaev17@gmail.com>
2025-11-28 06:22:05 +0600

    added buttons UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Button } from "./components/UI/button";
+
 const App = () => {
   return (
     <div>
-     
+      <Button variant="amber">Default</Button>
+      <Button variant="amber" disabled>
+        Disabled
+      </Button>
+
+      <Button className="w-[425px] h-[50px]" variant="google">
+        Default
+      </Button>
     </div>
   );
-}
+};
 
-export default App
+export default App;
