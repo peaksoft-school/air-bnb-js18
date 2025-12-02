eb85e2ab981e6e2ae9526cf9feb530a68a46592c
Alihan <alihanduishonkulov15@gmail>
2025-12-02 14:30:11 +0600

    Add Chip component

--- src/components/UI/ChipBtn.tsx
+++ src/components/UI/ChipBtn.tsx
@@ -1,51 +1,40 @@
 import type { ReactNode } from "react";
 
-type Variant = "default" | "hover" | "active";
-
-interface VariantButtonProps {
-  variant?: Variant;
+interface BtnProps {
   children: ReactNode;
   onClick?: () => void;
 }
 
-const ChipBtn = ({
-  variant = "default",
-  children,
-  onClick,
-}: VariantButtonProps) => {
-  const base =
-    "flex items-center gap-2 px-4 py-2 w-fit select-none transition-all duration-200 rounded-full";
-
-  const styles: Record<Variant, string> = {
-    default: "bg-[#f3f3f3] text-gray-400",
-    hover: "bg-[#c4c4c4] text-white",
-    active: "bg-[#a0a0a0] text-white",
-  };
-
+const ChipBtn = ({ children, onClick }: BtnProps) => {
   return (
-    <button className={`${base} ${styles[variant]}`} onClick={onClick}>
+    <button
+      onClick={onClick}
+      className="
+        flex items-center gap-2 px-4 py-2 w-fit select-none
+        transition-all duration-200
+        bg-[#f3f3f3] text-gray-400
+        hover:bg-[#c4c4c4] hover:text-white
+        active:bg-[#a0a0a0] active:text-white
+      "
+    >
       <span className="text-lg">×</span>
       <span>{children}</span>
     </button>
   );
 };
 
-export const VariantButton = ({
-  variant = "default",
-  children,
-  onClick,
-}: VariantButtonProps) => {
-  const base =
-    "flex items-center gap-2 px-4 py-2 w-fit select-none transition-all duration-200";
-
-  const styles: Record<Variant, string> = {
-    default: "bg-[#f3f3f3] text-gray-400",
-    hover: "bg-[#c4c4c4] text-white",
-    active: "bg-[#a0a0a0] text-white",
-  };
-
+export const VariantButton = ({ children, onClick }: BtnProps) => {
   return (
-    <button className={`${base} ${styles[variant]}`} onClick={onClick}>
+    <button
+      onClick={onClick}
+      className="
+        flex items-center gap-2 px-4 py-2 w-fit select-none
+        transition-all duration-200
+        bg-[#f3f3f3] text-gray-400
+        hover:bg-[#c4c4c4] hover:text-white
+        active:bg-[#a0a0a0] active:text-white
+      "
+    >
       <span className="text-lg">×</span>
       <span>{children}</span>
     </button>
