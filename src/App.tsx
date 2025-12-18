bcddf085b5905321757f34b0df6586ea4523976f
Ymanbek Baltabaev Eralievich <ymanbekbaltabaev17@gmail.com>
2025-12-18 18:38:25 +0600

    feat(ui): add modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import { Button } from "./components/UI/button";
 
 const App = () => {
   return (
-    <div>
-      <Button variant="amber">Default</Button>
-      <Button variant="amber" disabled>
-        Disabled
-      </Button>
-
-      <Button className="w-[425px] h-[50px]" variant="google">
-        Default
-      </Button>
-    </div>
+<div></div>     
   );
 };
 
