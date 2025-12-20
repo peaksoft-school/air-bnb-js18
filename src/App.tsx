52eafc314706d621a1d52f43589d6ceceed28c72
Ymanbek Baltabaev Eralievich <ymanbekbaltabaev17@gmail.com>
2025-12-20 18:47:34 +0600

    App.tsx brush up

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,3 @@
-import { useState } from "react"
-import { Button } from "./components/ui/button"
-import { Modal } from "./components/ui/modal"
+function App() {}
 
-function App() {
-  const [open, setOpen] = useState(false)
-
-  return (
-    <div className="p-10">
-      <Button onClick={() => setOpen(true)}>
-        Sign in
-      </Button>
-
-      <Modal
-        open={open}
-        onOpenChange={setOpen}
-        title="Delete item"
-        description="Are you sure you want to delete this item?"
-        confirmText="Delete"
-        cancelText="Cancel"
-      >
-        <p>Hello</p>
-      </Modal>
-    </div>
-  )
-}
-
-export default App
+export default App;
