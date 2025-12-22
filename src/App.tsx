2d6c1feb82c0927f868dfdd415c00b2d48b18c46
Rabiya <rabiyaaiylchieva@gmail.com>
2025-12-22 12:03:53 +0600

    clear App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,3 @@
-const App = () => {
-  return (
-    <div>
-      <h1 className="font-inter font-bold">Inter H1</h1>
-      <h1 className="font-jenriv font-light">Jenriv Light</h1>
-
-      <div className="bg-accent text-accent-foreground">Green block</div>
-
-      <div className="bg-chart-4">Pink accent</div>
-    </div>
-  );
-};
+const App = () => <h1>Air bnb JS-20</h1>;
 
 export default App;
