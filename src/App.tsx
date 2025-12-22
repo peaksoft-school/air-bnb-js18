6715aad4e59b037baf8ad95edb53a7cfae365380
Rabiya <rabiyaaiylchieva@gmail.com>
2025-12-22 11:55:14 +0600

    add themes color

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 const App = () => {
   return (
     <div>
-     
+      <h1 className="font-inter font-bold">Inter H1</h1>
+      <h1 className="font-jenriv font-light">Jenriv Light</h1>
+
+      <div className="bg-accent text-accent-foreground">Green block</div>
+
+      <div className="bg-chart-4">Pink accent</div>
     </div>
   );
-}
+};
 
-export default App
+export default App;
