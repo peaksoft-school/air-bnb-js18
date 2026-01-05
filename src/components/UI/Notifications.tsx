4009d574ee75ef315bca9296aecd7d5437099ae4
Rabiya <rabiyaaiylchieva@gmail.com>
2026-01-05 15:23:38 +0600

    save

--- src/components/UI/Notifications.tsx
+++ src/components/UI/Notifications.tsx
@@ -1,6 +1,32 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+export type NotificationVariant = "success" | "info" | "error" | "booked";
+
+interface NotificationLayoutProps {
+  variant: NotificationVariant;
+  title: string;
+  message: string;
+  onClose?: () => void;
+}
+
+interface NotificationLayoutProps {
+  variant: NotificationVariant;
+  title: string;
+  message: string;
+  onClose?: () => void;
+}
+
+const contentByVariant: Record<
+  NotificationVariant,
+  { bg: string; border: string }
+> = {
+  success: { bg: "bg-[#F0FFF1]", border: "border-[#DFF5E1]" },
+  info: { bg: "bg-[#fff8f0]", border: "border-[#f5eddf]" },
+  error: { bg: "bg-[#FFF1F0]", border: "border-[#f5dfdf]" },
+  booked: { bg: "bg-[#F0FFF1]", border: "border-[#DFF5E1]" },
+};
+
 export const Notification = () => {
   return (
     <ToastContainer
@@ -14,31 +40,12 @@ export const Notification = () => {
       draggable
       pauseOnHover
       limit={3}
-      className="max-w-[38rem] min-w-[20rem] w-fit"
+      className="max-w-152 min-w-[20rem] w-fit"
       toastClassName="bg-white border p-4 rounded shadow-lg flex flex-col"
       closeButton={false}
     />
   );
 };
-
-export type NotificationVariant = "unblocked" | "blocked" | "wrong" | "booked";
-
-const contentByVariant: Record<
-  NotificationVariant,
-  { bg: string; border: string }
-> = {
-  unblocked: { bg: "bg-[#F0FFF1]", border: "border-[#DFF5E1]" },
-  blocked: { bg: "bg-[#F0FFF1]", border: "border-[#DFF5E1]" },
-  wrong: { bg: "bg-[#FFF1F0]", border: "border-[#DFF5E1]" },
-  booked: { bg: "bg-[#F0FFF1]", border: "border-[#DFF5E1]" },
-};
-
-interface NotificationLayoutProps {
-  variant: NotificationVariant;
-  title: string;
-  message: string;
-  onClose?: () => void;
-}
 
 export function NotificationLayout({
   variant,
@@ -51,13 +58,13 @@ export function NotificationLayout({
     <div
       className={`
         relative
-        w-[612px]
-        ${variant === "wrong" ? "h-[100px]" : "h-[66px]"}
+        w-full
+        h-[66px]
         ${content.bg}
         ${content.border}
         border
-        px-[28px]
-        py-[12px]
+        px-7
+        py-3
         flex items-center
         animate-in
         slide-in-from-top-2
@@ -72,5 +79,3 @@ export function NotificationLayout({
     </div>
   );
 }
-
-export default Notification;
