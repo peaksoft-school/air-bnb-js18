24dacd8e0cbaf39a875228d72b93d3083a95d01a
Rabiya <rabiyaaiylchieva@gmail.com>
2026-01-05 13:23:03 +0600

    save

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,59 +1,40 @@
-import React from "react";
+import { forwardRef, type InputHTMLAttributes } from "react";
 import { cn } from "../../lib/utils";
 import { SearchIcon } from "lucide-react";
 
 type InputProps = {
-  type?: string;
-  placeholder?: string;
-  value?: string;
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
-  disabled?: boolean;
   icon?: boolean;
-} & React.InputHTMLAttributes<HTMLInputElement>;
+} & InputHTMLAttributes<HTMLInputElement>;
 
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  (
-    {
-      type = "text",
-      placeholder,
-      value,
-      onChange,
-      disabled,
-      icon,
-      className,
-      ...rest
-    },
-    ref
-  ) => {
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ icon, className, disabled, ...props }, ref) => {
     return (
       <div className="relative w-full">
         {icon && (
-          <span className="absolute left-3 top-1/2 -translate-y-1/2 text-[#C4C4C4]">
-            <SearchIcon />
+          <span className="pointer-events-none absolute left-3 top-1/2 -translate-y-1/2 text-[#C4C4C4]">
+            <SearchIcon size={18} />
           </span>
         )}
+
         <input
           ref={ref}
-          type={type}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
           disabled={disabled}
           className={cn(
-            "w-full h-9 px-3",
-            "text-[16px] font-normal leading-[19px]",
-            "text-[rgba(196,196,196,1)]",
-            "border border-[rgba(196,196,196,1)]",
-            "box-border",
+            "h-9 w-full rounded-md border px-3 text-base text-black",
+            "placeholder:text-[#C4C4C4]",
+            "border-[rgba(196,196,196,1)]",
+            "transition-colors",
             "hover:border-[rgba(130,130,130,1)]",
             "focus:border-[rgba(130,130,130,1)] focus:outline-none focus:ring-0",
-            "disabled:opacity-50 disabled:cursor-not-allowed",
+            "disabled:cursor-not-allowed disabled:opacity-50",
             icon && "pl-9",
             className
           )}
-          {...rest}
+          {...props}
         />
       </div>
     );
   }
 );
+
+Input.displayName = "Input";
