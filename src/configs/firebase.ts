36f19bee7f3a5cc69d7edfa0a2bc41bfaa5ce1f5
Rabiya <rabiyaaiylchieva@gmail.com>
2026-02-04 23:32:42 +0600

    fix firebase

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxMH2XqWOH3KZ72dRznVVrGXc0ycgqGDs",
-  authDomain: "airbnb-b18.firebaseapp.com",
-  projectId: "airbnb-b18",
-  storageBucket: "airbnb-b18.firebasestorage.app",
-  messagingSenderId: "909161573982",
-  appId: "1:909161573982:web:0049c61f6aea9be5fd0ecf",
-  measurementId: "G-MPXE6TKQ4B",
+  apiKey: "AIzaSyC7pYPtdxY-GrHBtktrcHxKlv78vqClswM",
+  authDomain: "airbnb-b11.firebaseapp.com",
+  projectId: "airbnb-b11",
+  storageBucket: "airbnb-b11.appspot.com",
+  messagingSenderId: "413730144303",
+  appId: "1:413730144303:web:56c5fd23d1f40b7a1506da",
+  measurementId: "G-FM2CPS77X8",
 };
 
 const app = initializeApp(firebaseConfig);
